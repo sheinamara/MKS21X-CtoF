d12248aa4a441705227143907297dc96b5bb4168
sheinamara <ashein00@stuy.edu>
2018-09-18 18:10:21 -0400

    tried to make working code

---
+++ CtoFTester.java
@@ -0,0 +1,8 @@
+public class CtoFTester{
+  public static int celsiusToFahrenheit(int C) {
+    return (9 / 5) * C + 32;
+  }
+  public static int fahrenheitToCelsius(int F){
+    return (5 / 9) * (F - 32);
+  }
+}
